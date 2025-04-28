93303e89048ae66dc1cd094591fb58c576f1e1f7
Ronald Maroria <ron.maroria@gmail.com>
2025-04-28 00:06:37 -0500

    random_string_generator - Advanced

--- random_string_generator.py
+++ random_string_generator.py
@@ -1,9 +1,13 @@
 import random
 import string
 
-number_of_names = int(input("How many names do you want to generate? "))
-department_names = input("What is the name of the department? ")
+number_of_names = int(input("How many names do you want to generate?\n"))
+user_department = input("What is your department?\n")
+department_names = ["Marketing", "Accounting", "FinOps"]
 
 for i in range(number_of_names):
-    random_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
-    print(f"{department_names}-{random_name}")
+        while user_department in department_names:
+              random_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+              print(user_department + "-" + random_name)
+        else:
+            print("Invalid department name")
