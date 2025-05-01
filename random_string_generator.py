7eff6200c2e02b6247dfa6fe0749b6394ed52da7
slicerpro <ron.maroria@gmail.com>
2025-04-30 22:17:03 -0500

    random string tweaks
    random string tweaks

--- random_string_generator.py
+++ random_string_generator.py
@@ -1,16 +1,21 @@
 import random
 import string
+import sys
 
 number_of_names = int(input("How many names do you want to generate?\n"))
 user_department = input("What is your department?: ")
 department_names = ["Marketing", "Accounting", "FinOps"]
 
 
-for i in range(number_of_names):
-        if user_department not in department_names:
-            print("\nPlease enter a valid department name\n")
-            break
-        else:
-            random_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
-            print(user_department + "-" + random_name)
+# Check if the department name is valid
+if user_department not in department_names:
+    
+    print("You have entered an invalid department name\n\n")
+   
+else:
+     print(f"The generated names are:\n")
+    # Generate random names
+     for i in range(number_of_names):
+        random_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+        print(user_department + "-" + random_name)
             
