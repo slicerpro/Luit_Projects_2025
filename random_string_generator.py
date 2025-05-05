0a08f922597d7f53baa9ad15ec2e84a4291f480e
slicerpro <ron.maroria@gmail.com>
2025-05-05 10:34:48 -0500

    Update random_string_generator.py

--- random_string_generator.py
+++ random_string_generator.py
@@ -1,6 +1,5 @@
 import random
 import string
-import sys
 
 number_of_names = int(input("How many names do you want to generate?\n"))
 user_department = input("What is your department?: ")
@@ -8,8 +7,7 @@ department_names = ["Marketing", "Accounting", "FinOps"]
 
 
 # Check if the department name is valid
-if user_department not in department_names:
-    
+if user_department not in department_names:    
     print("You have entered an invalid department name\n\n")
    
 else:
